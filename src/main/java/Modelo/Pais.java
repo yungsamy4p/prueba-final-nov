eb218d716d4b8e968c62d74d080427644c0be353
Yung Samy <113071996+yungsamy4p@users.noreply.github.com>
2025-11-27 09:31:40 -0300

    asd
    asd

---
+++ src/main/java/Modelo/Pais.java
@@ -0,0 +1,5 @@
+package Modelo;
+
+public class Pais {
+    //Acá codificará las Características y Propiedades para la clase País.
+}
---
+++ src/main/java/com/mycompany/datospaises/DatosPaises.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datospaises;
+
+/**
+ *
+ * @author Santo Tomas
+ */
+public class DatosPaises {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
